b8ff66e9b3a47c873c9d7ac0e38107084107bd2d
Sam Schantz <samvschantz@gmail.com>
2018-04-28 01:20:26 +0000

    Completed delete functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,21 +2,28 @@ class ReviewsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    @review = product.reviews.new(
+    review = product.reviews.new(
       user_id: current_user[:id],
       product_id: params[:product_id],
       description: params[:review][:description],
       rating: params[:review][:rating]
       )
-    if @review.save
+    if review.save
       redirect_to product
     else
       redirect_to '/products'
     end
   end
 
-  def delete
+  def destroy
+    product = Product.find(params[:product_id])
+    review = Review.find(params[:id])
     review.delete
+    if review.delete
+      redirect_to product
+    else
+      redirect_to '/products'
+    end
   end
 
 end
