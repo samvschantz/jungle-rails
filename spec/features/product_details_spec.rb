e893e004d630db2311da69fe92c5b54740f20410
Sam Schantz <samvschantz@gmail.com>
2018-05-01 19:46:50 +0000

    Added test for clicking through to details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG
+    # save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product', count: 10
+  end
+
+  scenario "They click through to details and see the description" do
+    visit root_path
+
+    first('article.product').find_link('Details').click
+
+    sleep 3
+
+    expect(page).to have_css 'article.product-detail'
+  end
+end
